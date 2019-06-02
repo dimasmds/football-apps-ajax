0ce43bc11ee5b63b9bae8fd45837ea55e7a22675
dicodingreviewer <dicodingreviewer@gmail.com>
2019-06-03 02:56:58 +0700

    Common content

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+const BASE_URL = 'https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=';
+var progressBar = ` <div class="progress">
+                        <div class="indeterminate"></div>
+                    </div>`;
+let content = document.getElementById("content");
+
 document.addEventListener("DOMContentLoaded", function () {
     let searchInput = document.getElementById("search-team")
     searchInput.addEventListener("keyup", function (event) {
@@ -6,10 +12,31 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     })
 });
-var progressBar = ` <div class="progress">
-                        <div class="indeterminate"></div>
-                    </div>`;
+
 
 function getTeam(teamName) {
 
+}
+
+function showTeam(data) {
+    let responseText = `<div class="row">`
+
+    if(data.teams !== null) {
+        data.teams.forEach(function (item) {
+            responseText += `<div class="col s12 m6 l4"> <div class="card">
+        <div class="card-image">
+          <img src="${item.strTeamBadge}">
+        </div>
+        <div class="card-content">
+          <span class="card-title">${item.strTeam}</span>
+          <p class="text-ellipsis">${item.strDescriptionEN}</p>
+        </div>
+      </div></div>`
+        });
+    } else {
+        responseText += `<p class="center">Data Not Found :(</p>`
+    }
+
+    responseText += `</div>`;
+    content.innerHTML = responseText;
 }
